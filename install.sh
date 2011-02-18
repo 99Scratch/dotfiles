dbd70033b70fdb6c4a7a72312e72810e7146e125
Strubbl <git-dev@linux4tw.de>
2011-02-18 14:42:06 +0100

    SciTE config into home and locale for German into /usr/share/scite if dir exists

--- install.sh
+++ install.sh
@@ -4,5 +4,11 @@ cp bash_aliases ~/.bash_aliases
 cp screenrc ~/.screenrc
 cp vimrc ~/.vimrc
 cp gitconfig  ~/.gitconfig
+cp SciTEUser.properties ~/.SciTEUser.properties
 cp -r bin ~
+if [ -d /usr/share/scite ] 
+then
+	sudo cp locale.de.properties /usr/share/scite/locale.properties
+#	echo "scite locale de copied"
+fi
 
