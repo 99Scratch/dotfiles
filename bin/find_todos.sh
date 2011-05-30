a175e4eafb2a21404e08993b612d19d407b693bb
Strubbl <git-dev@linux4tw.de>
2011-05-30 15:08:49 +0200

    bin/find_todos.sh: show only tex files with more than 0 todo-counts

--- bin/find_todos.sh
+++ bin/find_todos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 find . -name "*.tex" -exec grep -inrH --color \\\\todo\{.*\} {} \;
 echo "Counter:"
-grep -irc \\\\todo\{.*\} * | grep \.tex:
+grep -irc \\\\todo\{.*\} * | grep -v :0$ | grep \.tex:
