f7edd9da0bd15b5cc0604b023509d8b638201833
Sven Fischer <git-dev@linux4tw.de>
2016-11-02 08:24:35 +0100

    only move font in case of successful curl

--- install.sh
+++ install.sh
@@ -128,6 +128,5 @@ cp zshrc ~/.zshrc
 cp bullet-train-oh-my-zsh-theme/bullet-train.zsh-theme ~/.oh-my-zsh/themes/
 
 mkdir -p ~/.local/share/fonts
-curl -fLo "DejaVu Sans Mono for Powerline Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.ttf
-mv "DejaVu Sans Mono for Powerline Nerd Font Complete.ttf" ~/.local/share/fonts/
+curl -fLo "DejaVu Sans Mono for Powerline Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.ttf && mv "DejaVu Sans Mono for Powerline Nerd Font Complete.ttf" ~/.local/share/fonts/
 
