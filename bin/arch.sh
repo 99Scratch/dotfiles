1a97c945ddbed67a7b8ce8252df270e1ce9e74e7
Sven Fischer <git-dev@linux4tw.de>
2017-04-26 22:16:44 +0200

    arch.sh: differentiate between uid from caller

--- bin/arch.sh
+++ bin/arch.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
+set -u
+set -o xtrace
+trap "echo ABBRUCH ; exit 1" SIGINT
 
-pacman -Syu
+# as root
+if [ $UID -eq 0 ]
+then
+  useradd -m -G wheel -s /bin/bash strubbl
+  mkdir /home/strubbl
+  chown strubbl: /home/strubbl
+  passwd strubbl
+  pacman -Syu
+  pacman -S sudo
+  echo "%sudo	ALL=(ALL) ALL" >> /etc/sudoers
+  usermod -a -G sudo strubbl
 
-pacman -S --needed base-devel
+# as user
+else
+  sudo pacman -S --needed base-devel
 
-# non-gui
-pacman -S sudo git vim bash-completion cowsay fortune-mod openssh sshuttle mpv screen tmux curl wget go go-tools nmap bind-tools htop nethogs smartmontools usbutils vnstat zsh zsh-completions whois
+  # non-gui
+  sudo pacman -S sudo git vim bash-completion cowsay fortune-mod openssh sshuttle screen tmux curl wget go go-tools nmap bind-tools htop nethogs smartmontools usbutils vnstat zsh zsh-completions whois
 
-# GUI
-pacman -S i3-wm i3lock i3status mpv firefox pidgin pidgin-otr gimp libreoffice chromium file-roller gedit gnome-terminal gparted gwenview kile liteide meld midori okular opera screenfetch xnviewmp volumeicon
+  # GUI
+  sudo pacman -S i3-wm i3lock i3status mpv firefox pidgin pidgin-otr gimp libreoffice chromium file-roller gedit gnome-terminal gparted gwenview kile liteide meld midori okular opera screenfetch xnviewmp volumeicon mpv
 
-# Network Manager
-pacman -S networkmanager network-manager-applet
+  # Network Manager
+  sudo pacman -S networkmanager network-manager-applet
 
-# yaourt
+  # yaourt
+  mkdir ~/git && cd ~/git
+  git clone https://aur.archlinux.org/package-query.git
+  cd package-query
+  makepkg -si
+  cd ..
+  git clone https://aur.archlinux.org/yaourt.git
+  cd yaourt
+  makepkg -si
+  cd ..
 
-git clone https://aur.archlinux.org/package-query.git
-cd package-query
-makepkg -si
-cd ..
-git clone https://aur.archlinux.org/yaourt.git
-cd yaourt
-makepkg -si
-cd ..
+  yaourt -Syua
+  yaourt -S slurm cntlm visual-studio-code-oss
 
-yaourt -Syua
-yaourt -S slurm cntlm visual-studio-code-oss
-
-# TODO
-#ntp
-#ssh
+  # TODO
+  #ntp
+  #ssh
+fi
 
